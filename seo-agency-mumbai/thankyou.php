c33202a34be86cbcbb574fa6743fb34ebbe7acb9
Meenaxijb <meenaxi.badola@justwords.inm>
2023-08-17 10:32:04 +0530

    updated

---
+++ seo-agency-mumbai/thankyou.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thankyou</title>
+
+<meta charset="utf-8">	
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<!-- <meta http-equiv="refresh" content="3;url=http://www.idn.com/" /> -->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">	
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="assets/css/owl.theme.default.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<style type="text/css">
+.tkbg {background: #e5e5e5; padding: 20px 0;}	
+.centerdiv { width: 400px;  background: #fff; /*box-shadow: 0px 0px 10px 0px #f1f1f1;*/  margin: 20px auto ; padding: 30px 20px;  }
+.coreticon { color: #1fcb13; text-align: center; display: block; padding: 0px 0; font-size: 50px; }
+.thanks-hding { text-align: center; color: #635d5d; font-size: 48px; }
+.thanks-prgh { text-align: center; color:#635d5d; font-size: 18px; }
+.backtohome { font-size: 16px; text-align: center; color: #197dd2; padding: 20px 0; display:block; text-decoration: none; }
+.backtohome:hover { text-decoration: none; color: #197dd2; }
+.backtohome > i { color:#0060a3; position: relative; top: 1px; }
+.thankslogo { width: 220px; margin: 20px auto 10px; display: block; }
+@media only screen and (max-width:767px) {
+	.centerdiv {width: 90%;}
+}	
+</style>	
+
+</head>
+<body>
+<header id="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <ul class="top-right-menu">
+          <li>
+            <a href="https://www.justwords.in/fraud-disclaimer-alert/">Fraud Disclaimer Alert</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="d-flex align-items-center justify-content-between">
+      <div class="logo ps-0 d-block d-lg-none">
+        <img src="assets/img/logo.png" alt="">
+      </div>
+      <button type="button" class="navbar-toggle" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>  
+    <div class="d-flex align-items-center justify-content-center">
+      <div class="collapse navbar-collapse mobnav" id="navbarSupportedContent">
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li>
+              <a class="nav-link " href="/">Home</a>
+            </li>
+            <li>
+              <a class="nav-link " href="https://www.justwords.in/about-us/">About Us</a>
+            </li>
+            <li class="dropdown">
+              <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Our Services</a>
+              
+              <ul class="dropdown-menu megamenu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a href="#">Content</a>
+                  <ul class="megamenu-dropdown">
+                    <li><a href="https://www.justwords.in/content-writing-services/">Content Writing Service</a></li>
+                    <li><a href="https://www.justwords.in/content-marketing-services/">Content Marketing Services</a></li>
+                    <li><a href="https://www.justwords.in/content-marketing-agency/">CONTENT MARKETING AGENCY</a></li>
+                    <li><a href="https://www.justwords.in/content-strategy/">Content Strategy</a></li>
+                    <li><a href="https://www.justwords.in/seo-content-services/">SEO Content</a></li>
+                    <li><a href="https://www.justwords.in/social-media-content-services/">Social Media Content</a></li>
+                    <li><a href="#">Visual Content</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Digital</a>
+                  <ul class="megamenu-dropdown">
+                    <li><a href="https://www.justwords.in/digital-marketing-services/">Digital Marketing Services</a></li>
+                    <li><a href="https://www.justwords.in/social-media-marketing-services/">Social Media Marketing</a></li>
+                    <li><a href="https://www.justwords.in/video-marketing-services/">Video Marketing</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">SEO</a>
+                  <ul class="megamenu-dropdown">
+                    <li><a href="https://www.justwords.in/seo-services/">SEO Services</a></li>
+                    <li><a href="https://www.justwords.in/local-seo-services/">Local SEO Services</a></li>
+                    <li><a href="https://www.justwords.in/mobile-seo-services/">Mobile SEO Services</a></li>
+                    <li><a href="https://www.justwords.in/ecommerce-seo-services/">ECommerce SEO</a></li>
+                    <li><a href="https://www.justwords.in/international-seo-services/">International SEO</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Visual</a>
+                <ul class="megamenu-dropdown">
+                    <li><a href="https://www.justwords.in/video-marketing-services/">VIDEO</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a class="nav-link " href="https://www.justwords.in/seo-agency/">SEO Agency</a>
+            </li>
+          </ul>
+          <div class="logo d-none d-lg-block">
+            <img src="assets/img/logo.png" width="100%" alt="">
+          </div>
+          <ul>  
+            <li class="dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">Resources</a>
+              <ul class="dropdown-menu dark-dropdown" aria-labelledby="navbarDropdown2">
+                <li><a class="dropdown-item" href="https://www.justwords.in/blog/">Blog</a></li>
+                <li><a class="dropdown-item" href="https://www.justwords.in/ebook/">EBook</a></li>
+                <li><a class="dropdown-item" href="https://www.justwords.in/what-is-content-marketing/">What is Content Marketing</a></li>
+              </ul>
+            </li>
+            <li>
+              <a class="nav-link " href="https://www.justwords.in/careers/">Careers</a>
+            </li>
+            <li>
+              <a class="nav-link " href="https://www.justwords.in/contact-us/">Contact</a>
+            </li>
+            <li>
+              <a class="nav-link " href="https://www.justwords.in/all-work/">Our Work</a>
+            </li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+      
+    </div>  
+    <!-- .navbar -->
+  </div>
+</header>
+
+<!-- Start Thankyou Page -->
+<section class="tkbg">
+	<div class="centerdiv">
+		<p class="coreticon"><i class="far fa-check-circle"></i></p>
+		<h1 class="thanks-hding">Thank you!</h1>
+		<p class="thanks-prgh">Your submission is received and we will contact you soon.</p>
+		<img src="assets/img/logo.png" class="thankslogo" />
+		<a href="https://www.justwords.in/" class="backtohome"><i class="fas fa-long-arrow-alt-left"></i> Back to Home</a>
+	</div>
+</section>
+<!-- End Thankyou Page -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-md-3">
+        <h4 class="footer-title">ABOUT US</h4>
+        <p class="footer-text">We are an award-winning content agency with the focus of making the content sourcing process easy and affordable for everyone. We believe in delivering content that is packed with value, is engaging and works to get you business.</p>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <h4 class="footer-title">SERVICES</h4>
+        <ul class="footer-list">
+          <li><a href="https://www.justwords.in/content-writing-services/">CONTENT WRITING</a></li>
+          <li><a href="https://www.justwords.in/content-strategy/">CONTENT STRATEGY</a></li>
+          <li><a href="https://www.justwords.in/content-marketing-services/">CONTENT MARKETING</a></li>
+          <li><a href="https://www.justwords.in/seo-services/">SEO</a></li>
+          <li><a href="https://www.justwords.in/digital-marketing-services/">DIGITAL MARKETING</a></li>
+          <li><a href="https://www.justwords.in/social-media-marketing-services/">SOCIAL MEDIA MARKETING</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <h4 class="footer-title">COMPANY</h4>
+        <ul class="footer-list">
+          <li><a href="https://www.justwords.in/about-us/">ABOUT US</a></li>
+          <li><a href="https://www.justwords.in/careers/">CAREERS</a></li>
+          <li><a href="https://www.justwords.in/all-work/">OUR WORK</a></li>
+          <li><a href="https://www.justwords.in/contact-us/">CONTACT US</a></li>
+          <li><a href="https://www.justwords.in/blog/">BLOG</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <h4 class="footer-title">LOCATIONS</h4>
+        <ul class="footer-list">
+          <li><a href="javascript:void(0)">Gurgaon <i class="fas fa-caret-down"></i></a>
+            <ul>
+              <li><a href="https://www.justwords.in/seo-agency-gurgaon/">SEO AGENCY IN GURGAON</a></li>
+              <li><a href="https://www.justwords.in/digital-marketing-services-gurgaon/">DIGITAL MARKETING AGENCY IN GURGAON</a></li>
+              <li><a href="https://justwords.in/social-media-agency-in-gurgaon/">SOCIAL MEDIA AGENCY IN GURGAON</a></li>
+
+            </ul>
+          </li>
+          <li><a href="javascript:void(0)">Mumbai <i class="fas fa-caret-down"></i></a>
+                <ul>
+                  <li><a href="https://www.justwords.in/seo-agency-mumbai/">SEO AGENCY IN MUMBAI</a></li>
+                </ul>
+              </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row ftbot-border justify-content-center">
+      <div class="col-sm-10">
+        <div class="d-flex justify-content-center align-items-center">
+          <ul class="social-icons-footer">
+            <li><a href="https://www.facebook.com/justwords.in" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="https://twitter.com/justwords_in" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.youtube.com/c/JustwordsIn" target="_blank"><i class="fab fa-youtube"></i></a></li>
+            <li><a href="https://www.linkedin.com/company/justwords/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://www.instagram.com/justwords_in/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+          </ul>
+          <div class="subscription_form_footer">
+            <form action="" method="post">
+              <div class="row">
+                <div class="col-sm-6">
+                  <input type="text" name="subscription" class="subscription-field" placeholder="Enter Your Email">
+                </div>
+                <div class="col-sm-4">
+                  <button type="submit" name="submit" class="subscription-btn">Subscribe Now</button>    
+                </div>
+              </div>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-9">
+          <p class="footer-copyright-text"> 
+            © 2021 Justwords, Inc. All rights reserved | <a href="https://www.justwords.in/privacy-policy/">Privacy Policy</a></p>
+        </div>
+        <div class="col-sm-3">
+          <a href="#">
+            <img src="assets/img/footer-logo.png" alt="" class="footer-logo">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>  
+</footer>
+<!-- End Footer -->
+
+
+</body>
+</html>
